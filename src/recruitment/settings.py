eab9fa6cff39400fca0005aca0564b075ce9db45
Vladyslav Tertyshnyi <vladyslav.tertyshnyi@eleks.com>
2018-03-01 11:32:57 +0200

    Add rest framework

--- src/recruitment/settings.py
+++ src/recruitment/settings.py
@@ -26,7 +26,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'vacancy'
+
+    'vacancy',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -88,6 +90,14 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 
 # Internationalization
