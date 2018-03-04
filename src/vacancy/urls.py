01ee134d0c8e034184643b9af2a1ae8fdf3d40fe
Vladyslav Tertyshnyi <vladyslav.tertyshnyi@eleks.com>
2018-03-04 18:33:15 +0200

    Add list view for vacancies

--- src/vacancy/urls.py
+++ src/vacancy/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from .views import VacancyRetrieveView, VacancyCreateView
+from .views import VacancyRetrieveView, VacancyListView
 
 urlpatterns = [
     url(r'^(?P<pk>\d+)/$', VacancyRetrieveView.as_view(), name='vacancy'),
-    url(r'^$', VacancyCreateView.as_view(), name='create-vacancy')
+    url(r'^$', VacancyListView.as_view(), name='create-vacancy')
 ]
--- src/vacancy/views.py
+++ src/vacancy/views.py
@@ -3,6 +3,20 @@ from rest_framework import generics
 from .models import Vacancy
 from .serializers import VacancySerializer
 
+class VacancyCreateView(generics.CreateAPIView):
+    lookup_field = 'pk'
+    serializer_class = VacancySerializer
+
+    def get_queryset(self):
+        return Vacancy.objects.all()
+
+class VacancyListView(generics.ListAPIView):
+    lookup_field = 'pk'
+    serializer_class = VacancySerializer
+
+    def get_queryset(self):
+        return Vacancy.objects.all()
+
 class VacancyRetrieveView(generics.RetrieveUpdateDestroyAPIView):
     lookup_field = 'pk'
     serializer_class = VacancySerializer
@@ -10,10 +24,5 @@ class VacancyRetrieveView(generics.RetrieveUpdateDestroyAPIView):
     def get_queryset(self):
         return Vacancy.objects.all()
 
-class VacancyCreateView(generics.CreateAPIView):
-    lookup_field = 'pk'
-    serializer_class = VacancySerializer
 
-    def get_queryset(self):
-        return Vacancy.objects.all()
 
