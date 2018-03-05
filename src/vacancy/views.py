7000f653ba51373ac4f030a8ed571c0df869b2eb
Vladyslav Tertyshnyi <vladyslav.tertyshnyi@eleks.com>
2018-03-05 21:28:52 +0200

    Add post method to vacancy end point

--- src/vacancy/views.py
+++ src/vacancy/views.py
@@ -1,21 +1,20 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, mixins
 from .models import Vacancy
 from .serializers import VacancySerializer
 
-class VacancyCreateView(generics.CreateAPIView):
+class VacancyListView(mixins.CreateModelMixin, generics.ListAPIView):
     lookup_field = 'pk'
     serializer_class = VacancySerializer
 
     def get_queryset(self):
         return Vacancy.objects.all()
-
-class VacancyListView(generics.ListAPIView):
-    lookup_field = 'pk'
-    serializer_class = VacancySerializer
-
-    def get_queryset(self):
-        return Vacancy.objects.all()
+    
+    # def perform_create(self, serializer):
+    #     serializer.save(user=self.request.user)
+    
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 class VacancyRetrieveView(generics.RetrieveUpdateDestroyAPIView):
     lookup_field = 'pk'
